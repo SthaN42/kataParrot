8d3475627c3ba62e79efa52d5f1b93852d3161f1
nathan guerin <nathan.guerin@etu.unilim.fr>
2021-11-17 10:57:23 +0100

    added polymorphism to european parrot

--- src/main/java/fr/unilim/iut/kataparrot/Parrot.java
+++ src/main/java/fr/unilim/iut/kataparrot/Parrot.java
@@ -3,7 +3,7 @@ package fr.unilim.iut.kataparrot;
 public class Parrot {
 
 	private ParrotTypeEnum type;
-	private int numberOfCoconuts = 0;
+	protected int numberOfCoconuts = 0;
 	private double voltage;
 	private boolean isNailed;
 
@@ -17,7 +17,7 @@ public class Parrot {
 	public double getSpeed() {
 		switch (type) {
 		case EUROPEAN:
-			return getBaseSpeed();
+			throw new RuntimeException("Should be unreachable");
 		case AFRICAN:
 			return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
 		case NORWEGIAN_BLUE:
@@ -30,11 +30,11 @@ public class Parrot {
 		return Math.min(24.0, voltage * getBaseSpeed());
 	}
 
-	private double getLoadFactor() {
+	protected double getLoadFactor() {
 		return 9.0;
 	}
 
-	private double getBaseSpeed() {
+	protected double getBaseSpeed() {
 		return 12.0;
 	}
 
--- src/test/java/fr/unilim/iut/kataparrot/ParrotTest.java
+++ src/test/java/fr/unilim/iut/kataparrot/ParrotTest.java
@@ -7,13 +7,13 @@ public class ParrotTest {
 
 	@Test
 	public void getSpeedOfEuropeanParrot() {
-		Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+		Parrot parrot = new EuropeanParrot();
 		assertEquals(12.0, parrot.getSpeed(), 0.0);
 	}
 
 	@Test
 	public void getSpeedOfAfricanParrot_With_One_Coconut() {
-		Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
+		Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
 		assertEquals(3.0, parrot.getSpeed(), 0.0);
 	}
 
